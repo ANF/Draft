d4f3d9dfa3d7bc18716b57020b2358ae60807c4e
ANF-Studios <theanfstudios@gmail.com>
2020-08-29 05:31:56 +0500

    Uploaded Project!
    This is the very first version of ListManager 0.1. It is missing very crucial features. But it is in alpha right now. You can use it!

---
+++ Scripts/Commands.cs
@@ -0,0 +1,116 @@
+using System;
+using System.IO;
+using System.Linq;
+using ListManager.Core;
+using System.Collections.Generic;
+
+namespace ListManager
+{
+    public class Cmds
+    {
+        public static void Commands()
+        {
+            string Command = Console.ReadLine();
+            switch (Command)
+            {
+                case "-Help":
+                case "-help":
+                case "-cmds":
+                case "-commands":
+                    Console.Clear();
+                    Console.WriteLine(
+@"Here are a list of available commands:
+                
+create/add
+Creates a list with specifed name and items
+
+view
+Shows a list of saved lists
+
+list/open
+Shows the saved list
+
+version
+Shows the version of application currently running
+
+Note: All commands must start the prefix '-'");
+                    Commands();
+                    break;
+
+                case "-v":
+                case "-version":
+                    Console.Clear();
+                    Console.WriteLine("This is List Manager running v0.1aot");
+                    Commands();
+                    break;
+
+                case "-create":
+                case "-add":
+                    Console.Clear();
+                    CreationInitialize();
+                    break;
+
+                case "-view":
+                    ViewList.ListNames();
+                    break;
+
+                case "-list":
+                case "-open":
+                    Console.WriteLine("What is the name of the list?");
+                    string name = Console.ReadLine();
+                    ListViewInitialize(name);
+                    break;
+
+                default:
+                    Console.WriteLine($"{Command} is not valid or recognized!");
+                    Commands();
+                    break;
+            }
+        }
+
+        private static void ListViewInitialize(string choice)
+        {
+            string[] GetFiles = Directory.GetFiles(@"C:\Users\Public\Documents\ListManager", "*.list");
+            List<string> Possible = new List<string>();
+
+            foreach(string ListToSep in GetFiles)
+            {
+                string GetFilesFinal = Path.GetFileNameWithoutExtension(ListToSep);
+                Possible.Add(GetFilesFinal);
+                //Console.WriteLine(GetFilesFinal);
+            }
+            
+            if(Possible.Contains(choice))
+            {
+                Console.WriteLine("Opening file!");
+                ViewList.ListView(choice);
+            }
+
+            else
+            {
+                Console.WriteLine("No such file found! Run the command again!");
+                Commands();
+            }
+        }
+
+        private static void CreationInitialize()
+        {
+            Console.WriteLine("What should be the name of the list?");
+            string listName = Console.ReadLine();
+
+            string[] GetFiles = Directory.GetFiles(@"C:\Users\Public\Documents\ListManager");
+            foreach (string file in GetFiles)
+            {
+                var GetFilesWE = Path.GetFileNameWithoutExtension(file);
+
+                if (GetFilesWE == listName)
+                {
+                    Console.WriteLine("Using this name will override the current list with this name");
+                    CreationInitialize();
+                }
+            }
+            GC.Collect();
+            ListCreator.ListCreation(listName);
+        }
+    }
+}
---
+++ Scripts/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace ListManager
+{
+    class Program
+    {
+        static void Main()
+        {
+            Initialize();
+            MoveToCmds();
+
+            //Beep if it reaches here for some reason
+            Console.Beep();
+            Console.ReadKey();
+        }
+
+        static void MoveToCmds()
+        {
+            Cmds.Commands();
+            System.Threading.Tasks.Task.Delay(-1).Wait();
+        }
+
+        static void Initialize()
+        {
+            if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Length > 1)
+            {
+                Console.WriteLine("Another session is already running! Terminating in 5 seconds");
+                System.Threading.Thread.Sleep(5000);
+                Process.GetCurrentProcess().Kill();
+            }
+            
+            else
+            {
+                Console.Title = "List Manager";
+                var rnd = new Random()
+                .Next(250, 1250);
+
+                Console.Write("Initializing..");
+                System.Threading.Thread.Sleep(rnd);
+                Console.Clear();
+                Console.WriteLine("Welcome to the list manager");
+                Console.WriteLine("To view a list of commands, use \"-help\"");
+                GC.Collect();
+                MoveToCmds();
+            }
+        }
+    }
+}
