c63772cb6a5e2647825971c87d88523ef7bdd7cb
ANF-Studios <theanfstudios@gmail.com>
2020-12-15 15:49:20 -0500

    Added iofunctions.js

--- src/components/iofunctions.js
+++ src/components/iofunctions.js
@@ -2,28 +2,56 @@
 const dialog = require('electron').dialog
 const fs = require('fs')
 const path = require('path')
-const documents = require('os').homedir()
+
+function checkForCommands() {
+    if (event.ctrlKey && event.key == "s") {
+        fs.writeFile("./Documents.pad", document.getElementById("pad").value, function (err) {
+            if (err) {
+                alert(err);
+                throw err;
+            }
+        });
+    }
+
+    if (event.ctrlKey && event.key == "o") {
+        fs.readFile("./Documents.pad", function (err, data) {
+            if (err) {
+                alert(err);
+                throw err;
+            }
+            document.getElementById("pad").value = data;
+        });
+    }
+}
+
+/*
+const dialogPopup = electron.dialog;
+  console.log(document.getElementById("pad").value)
+  dialogPopup.showSaveDialog().then(file => {
+    // Stating whether dialog operation was cancelled or not.
+    if (!file.canceled) {
+      console.log(file.filePath.toString());
+
+      // Creating and Writing to the sample.txt file
+      fs.writeFile(file.filePath.toString(),
+        'This is a Sample File', function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+        });
+    }
+  }).catch(err => {
+    console.log(err)
+  });}
 
 function ctrlSPressed() {
     if (event.ctrlKey && event.key == "s") {
         console.log(document.getElementById("pad").value)
-        electron.dialog.showSaveDialog({
-            title: 'Save the pad as a file.',
-            defaultPath: `${documents}/Documents`,
-            buttonLabel: 'Save',
-            // Restricting the user to only Text Files. 
-            filters: [
-                {
-                    name: 'Text Files',
-                    extensions: ['pad', 'txt']
-                },],
-            properties: []
-        }).then(file => {
+        dialog.showSaveDialog().then(file => {
             // Stating whether dialog operation was cancelled or not.
             if (!file.canceled) {
                 console.log(file.filePath.toString());
 
-                // Creating and Writing to the sample.txt file 
+                // Creating and Writing to the sample.txt file
                 fs.writeFile(file.filePath.toString(),
                     'This is a Sample File', function (err) {
                         if (err) throw err;
@@ -35,3 +63,36 @@ function ctrlSPressed() {
         });
     }
 }
+
+
+function ctrlSPressed() {
+    if (event.ctrlKey && event.key == "s") {
+        console.log(document.getElementById("pad").value)
+        electron.dialog.showSaveDialog({
+            title: 'Save the pad as a file.',
+            defaultPath: `${homedir}/Documents`,
+            buttonLabel: 'Save',
+            // Restricting the user to only Text Files.
+            filters: [
+                {
+                    name: 'Text Files',
+                    extensions: ['pad', 'txt']
+                },],
+            properties: []
+        }).then(file => {
+            // Stating whether dialog operation was cancelled or not.
+            if (!file.canceled) {
+                console.log(file.filePath.toString());
+
+                // Creating and Writing to the sample.txt file
+                fs.writeFile(file.filePath.toString(),
+                    'This is a Sample File', function (err) {
+                        if (err) throw err;
+                        console.log('Saved!');
+                    });
+            }
+        }).catch(err => {
+            console.log(err)
+        });
+    }
+}*/
