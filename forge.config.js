9d9f22a66e9c7404701e82204e7d5c174b513c8f
ANF-Studios <theanfstudios@gmail.com>
2020-12-15 06:46:08 -0500

    Added electron-forge configuration

---
+++ forge.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  "packagerConfig": {
+    "icon": path.join(__dirname, "src", "images", "favicon.ico"),
+    //"platform": "linux" // Choices are 'darwin', 'mas', 'win32' or 'linux'
+    //"arch": "arm64" // Choices are ia32, x64, armv7l, arm64
+  },
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "setupIcon": path.join(__dirname, "src", "images", "favicon.ico"),
+        "iconUrl": "https://raw.githubusercontent.com/ANF-Studios/ANFPad/master/src/images/favicon.ico",
+          "name": "ANFPad",
+          "authors": "ANF-Studios",
+          "description": "A simple application that manages all your notes.",
+          "noMsi": "true",
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": ['darwin', 'linux'],
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {}
+    }
+  ]
+}
